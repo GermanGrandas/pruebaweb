7d040fc1e44511eeb4550a3bbb2645f4ea7ee879
GermanGrandas <gertiven@gmail.com>
2017-08-05 12:27:10 -0500

    Add files via upload
    some Updates

---
+++ js/main.js
@@ -0,0 +1,43 @@
+"use strict";
+
+!function() {
+    let sticky = false;
+    $(document).ready(function(){
+        $("#navlow").removeClass("hidden");
+        $("#navlow").slideUp(0);
+    });
+
+
+    $(window).scroll(function() {
+        const inBottom = isInBottom();
+        if(inBottom && !sticky){
+            sticky=true;
+            stickNav(sticky);
+        }
+        if(!inBottom && sticky){
+            sticky=false;
+            stickNav(sticky);
+        }
+    });
+
+    function stickNav(sticky) {
+        if(sticky){
+            $("#description").addClass("fixed").removeClass("absolute");
+            $("#navigation").slideUp("fast");
+            $("#navlow").slideDown("fast");
+            /*$("#navigation").addClass("hidden");
+            $("#navlow").removeClass("hidden");*/
+        }else{
+            $("#description").removeClass("fixed").addClass("absolute");
+            $("#navigation").slideDown("fast");
+            $("#navlow").slideUp("fast");
+            /*$("#navigation").removeClass("hidden");
+            $("#navlow").addClass("hidden");*/
+        }
+    };
+    function isInBottom() {
+        const $description = $("#description")
+        const descriptionHeight = $description.height()
+        return $(window).scrollTop() > $(window).height() -descriptionHeight*2;
+    }
+}();
